98aa30cc9c5e146980ce8dda7d2732529d3a4ee0
m-wiemers <marcel@klebauter.de>
2021-01-29 15:13:07 +0100

    change logo to header

--- src/components/logo/logo.stories.js
+++
@@ -1,9 +0,0 @@
-import "./logo.css";
-import mainLogo from "./logo.html";
-
-export default {
-  title: "Components/Logo",
-  parameters: { layout: "centered" },
-};
-
-export const Logo = () => mainLogo;
