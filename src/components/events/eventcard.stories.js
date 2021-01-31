d5e17e8cecaa3946830e9e7fc555479a15a7c7c9
fre-ben <fschocke@gmail.com>
2021-01-31 16:57:00 +0100

    Add footer, add eventcard, add FontAwesome icons, removed folders in storybook, modified some existing files, add images

---
+++ src/components/events/eventcard.stories.js
@@ -0,0 +1,9 @@
+import "./events.css";
+import eventCard from "./eventcard.html";
+
+export default {
+  title: "Components/Events",
+  parameters: { layout: "centered" },
+};
+
+export const eventcard = () => eventCard;
---
+++ src/components/footer/footer.stories.js
@@ -0,0 +1,9 @@
+import "./footer.css";
+import mainFooter from "./footer.html";
+
+export default {
+  title: "Components/Footer",
+  parameters: { layout: "fullscreen" },
+};
+
+export const footer = () => mainFooter;
